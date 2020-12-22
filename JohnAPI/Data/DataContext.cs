082e603cbcead4436d1b2523665931b196fa71cc
88v <msanpietro@gmail.com>
2020-12-21 19:43:41 -0500

    Working web API

---
+++ JohnAPI/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using JohnAPI.Models;
+
+//HireMe@JohnSP.com
+
+namespace JohnAPI.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
